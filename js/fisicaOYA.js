1559fe5c6a5a2cd3ed922902d5ce7e223a2c637e
AndresFDaza <andresdaza04@hotmail.com>
2019-11-14 00:57:06 -0500

    se genera el sonido sinusoidal del piano por medio del click, falta animar la opturacion de las teclas

--- js/fisicaOYA.js
+++ js/fisicaOYA.js
@@ -1,7 +1,11 @@
+// ILUSTRACION Y ANIMACION TECLAS DE PIANO
+
 const teclas = document.querySelectorAll(".tecla"),
   note = document.querySelector(".oprimiendo"),
   pista = document.querySelectorAll(".pista");
 
+
+//obtener tecla presionada, generar animacion y mostrar nota musical
 function oprimeTecla(e) {
   const tecla = document.querySelector(`.tecla[data-tecla="${e.keyCode}"]`);
 
@@ -13,19 +17,35 @@ function oprimeTecla(e) {
   note.innerHTML = keyNote;
 }
 
+// remueve el loop de animaccion en teclas
 function removeTransition(e) {
   if (e.propertyName !== "transform") return;
   this.classList.remove("oprime");
 }
 
+//anima, estiliza nota musical presionada
 function muestraPista(e, index) {
   e.setAttribute("style", "transition-delay:" + index * 50 + "ms");
 }
 
+//genera loops y events listeners
 pista.forEach(muestraPista);
 
 teclas.forEach(tecla => tecla.addEventListener("transitionend", removeTransition));
 
 window.addEventListener("keydown", oprimeTecla);
 
-console.log(oprime);
+// SONIDOS SINUSOIDALES TECLAS DE PIANO
+
+var context= new AudioContext();
+ 
+function Sonido(frecuencia){
+        var oscilacion = context.createOscillator();
+        aumento=context.createGain();
+        oscilacion.connect(aumento);
+        oscilacion.type="sine";
+        oscilacion.frequency.value=frecuencia;
+        aumento.connect(context.destination);
+        oscilacion.start(0);
+        aumento.gain.exponentialRampToValueAtTime(0.00001,context.currentTime + .5);
+    }
